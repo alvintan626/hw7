1f7873674fe322396ee2485a51e7146a696d066e
Alvin Tan <alvintan626@berkeley.edu>
2018-03-06 16:42:11 -0800

    Update meeting.ino

--- meeting.ino
+++ meeting.ino
@@ -1,1 +1,24 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int brightness = 0 ;
+int fadeAmount = 5 ; 
 
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  
+  for (int i = 0; i < 5; i++){
+  digitalWrite(ledPin, HIGH);
+  delay(200);
+  digitalWrite(ledPin, LOW);
+  delay(200);
+  }
+  
+  delay(5000);
+}
