0a8e237a9a8d72c967b9dce8cf3a43d62cce1c84
Alvin Tan <alvintan626@berkeley.edu>
2018-03-06 16:41:43 -0800

    Update doctor.ino

--- doctor.ino
+++ doctor.ino
@@ -1,1 +1,26 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int brightness = 0 ;
+int fadeAmount = 5 ; 
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // set the brightness of pin 13:
+  analogWrite(ledPin, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  // wait for 30 milliseconds to see the dimming effect
+  delay(100);
+}
 
